dff26a5e5c02a02bfbdead451cdf08f6d2923edd
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-05-02 13:08:06 +0300

    Add files via upload

---
+++ HomeWork4.py
@@ -0,0 +1,7 @@
+immutable_var = 1, True, 'Кортеж', 'Не список'
+print(immutable_var)
+#immutable_var[1]=False
+mutable_list = [1, True,'Список','Не кортеж']
+print(mutable_list)
+mutable_list.append(333)
+print(mutable_list)
