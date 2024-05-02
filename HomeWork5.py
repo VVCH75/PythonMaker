08874a2303fac7eea5f64917f115bfd7746848ae
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-05-02 15:00:09 +0300

    Add files via upload

---
+++ HomeWork5.py
@@ -0,0 +1,14 @@
+my_list = ['Яблоко','Персик','Арбуз','Дыня','Груша']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:6])
+my_list[2]='Вишня'
+print(my_list)
+my_dict={'Яблоко':'Apple','Персик':'Peach','Арбуз':'Watermelon','Дыня':'Melon','Груша':'Pear'}
+print(my_dict)
+print(my_dict['Арбуз'])
+my_dict["Яблоко"]="Тыблако"
+print(my_dict)
+my_dict['Клубника'] = 'Strawberry'
+print(my_dict)
