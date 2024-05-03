43fa850e8a29ae09a83118e5705702ee9d3139ae
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-05-03 20:01:11 +0300

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,8 @@
+def print_params(parametr_):
+    print(parametr_)
+    print(parametr_)
+
+
+print_params('Конечный параметр')
+print_params('Промежуточный параметр')
+print_params('Начальный параметр')
