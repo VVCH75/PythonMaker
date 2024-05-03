275351cc1321c0658356cd6fcf264fbf4a2d0a95
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-05-03 17:09:19 +0300

    Add files via upload

---
+++ HomeWorkFor.py
@@ -0,0 +1,6 @@
+cars_count = 0
+list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in range(len(list_)):
+    print('Я езжу на автомабиле марки', list_[i])
+    cars_count += 10
+    print(cars_count)
