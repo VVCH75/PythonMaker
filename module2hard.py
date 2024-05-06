b117378cc9496fe4affed0c1e49c59868aba1105
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-05-06 11:02:38 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,10 @@
+list_ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+for i in range(3,21):
+    code_= ''
+    for j in range(0,19):
+        for k in range(1,19):
+            if i % (list_[j]+list_[k]) == 0 and list_[j] != list_[k] and code_.find(str(list_[k]) + str(list_[j])) < 0:
+                pare = str(list_[j])+str(list_[k])
+                code_= code_ + pare
+
+    print('Код для последовательности ',i,'- ',code_)
