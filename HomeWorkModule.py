ae28d903f1913b159bcc7c8384e6b3a7ffbaafaf
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-05-14 16:51:38 +0300

    Add files via upload

---
+++ HomeWorkModule.py
@@ -0,0 +1,7 @@
+def test_func():
+    print('Hello, ')
+def test_func1():
+    print('world!')
+
+if __name__ == '__test_func1()__':
+    main()
---
+++ main.py
@@ -0,0 +1,9 @@
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import HomeWorkModule
+from HomeWorkModule import test_func as ddd
+ddd()
+print('это привет по басурмански')
+from HomeWorkModule import test_func1 as ppp
+ppp()
+print('это мир по басурмански')
