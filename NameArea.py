ad7ff8da159a4215457a423e7cc05c1dfddfbb22
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-05-17 17:45:23 +0300

    Add files via upload

---
+++ NameArea.py
@@ -0,0 +1,7 @@
+def inner_function(args):
+    print(args)
+def test_function(args):
+    inner_function('Я в области видимости функции test_function')
+
+
+inner_function('Я вне области видимости функции test_function')
