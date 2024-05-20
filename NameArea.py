2840c7b5e82278f00a2359c200b2c2a19eb041fd
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-05-20 12:11:43 +0300

    Update NameArea.py
    Доработка

--- NameArea.py
+++ NameArea.py
@@ -1,7 +1,6 @@
 def inner_function(args):
     print(args)
-def test_function(args):
-    inner_function('Я в области видимости функции test_function')
-
+    def test_function(args):
+        inner_function('Я в области видимости функции test_function')
 
 inner_function('Я вне области видимости функции test_function')
