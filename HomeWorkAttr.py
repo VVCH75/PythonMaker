4945b4f0aa5722ad57831b97b0b1a4034567c51c
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-05-21 11:15:07 +0300

    Add files via upload

---
+++ HomeWorkAttr.py
@@ -0,0 +1,8 @@
+class hause():
+    numberOfFloors = 10
+
+for i in range(hause.numberOfFloors + 1):
+    if i == 0:
+        print('Мы находимся на подземной парковке')
+    else:
+        print('Мы находимся на ', i , ' этаже')
