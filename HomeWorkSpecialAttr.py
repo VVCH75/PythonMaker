ef694d8b002c25f271dc604902c1992b197745a0
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-05-24 15:57:14 +0300

    Add files via upload

---
+++ HomeWorkSpecialAttr.py
@@ -0,0 +1,10 @@
+class House:
+    def __init__(self, numberOfFloors):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(floors):
+        numberOfFloors = floors
+        print(numberOfFloors)
+
+
+House.setNewNumberOfFloors(10)
