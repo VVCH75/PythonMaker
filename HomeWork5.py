ed4d864146241447411c627799aa69903c3f1c73
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-07-03 16:31:50 +0300

    Update HomeWork5.py

--- HomeWork5.py
+++ HomeWork5.py
@@ -1,14 +1,6 @@
-my_list = ['Яблоко','Персик','Арбуз','Дыня','Груша']
-print(my_list)
-print(my_list[0])
-print(my_list[-1])
-print(my_list[2:6])
-my_list[2]='Вишня'
-print(my_list)
-my_dict={'Яблоко':'Apple','Персик':'Peach','Арбуз':'Watermelon','Дыня':'Melon','Груша':'Pear'}
-print(my_dict)
-print(my_dict['Арбуз'])
-my_dict["Яблоко"]="Тыблако"
-print(my_dict)
-my_dict['Клубника'] = 'Strawberry'
-print(my_dict)
+immutable_var = 1, True, 'Кортеж', 'Не список'
+print(immutable_var)
+mutable_list = [1, True,'Список','Не кортеж']
+print(mutable_list)
+mutable_list.append(333)
+print(mutable_list)
