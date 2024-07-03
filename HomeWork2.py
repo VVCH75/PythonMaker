93166b2dfb8d6e1af85e255c49b1c513b5ed2fd3
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-07-03 16:08:57 +0300

    Update HomeWork2.py

--- HomeWork2.py
+++ HomeWork2.py
@@ -6,3 +6,9 @@ age = age + 1
 print(age)
 is_student = True
 print(is_student)
+
+NumberDZ = 12
+Hours = 1.5
+NameOfCourse = 'Python'
+TimeOfOneTask = 1.5 / 12
+print('Курс: ', NameOfCourse, ', всего задач: ',NumberDZ, ', затрачено часов: ', Hours, ', среднее время выполнения ', TimeOfOneTask, ' часа.',sep='')
