bcd548712360bc7cf85d1ffc78fbe5ff39f30341
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-08-14 16:22:16 +0300

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,25 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters.command import Command
+import asyncio
+
+api = '7285833454:AAEvGzKEMaKVv-WOedGezrAPPFt0NO3-X2M'
+bot = Bot(token=api)
+dp = Dispatcher(storage=MemoryStorage())
+
+
+@dp.message(Command('start'))
+async def start_message(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message()
+async def message(message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+asyncio.run(main())
