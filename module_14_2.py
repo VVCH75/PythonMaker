8a66ec5ad66426397ae332c7698580b7f9978f8c
VVCH75 <168634860+VVCH75@users.noreply.github.com>
2024-08-21 10:42:17 +0300

    Add files via upload

---
+++ module_14_2.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
+
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+                   (f'User{i}', f'example{i}@gmail.com', i * 10, 1000))
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE Users SET balance = ? WHERE Username = ?', (500, f'User{i}'))
+
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE Username = ?', (f'User{i}',))
+
+# cursor.execute('SELECT Username, email, age, balance FROM Users WHERE age != ?', (60,))
+# users = cursor.fetchall()
+# for user in users:
+#     print(user)
+cursor.execute('DELETE FROM Users WHERE id = ?', (6,))
+cursor.execute('SELECT COUNT(*) FROM Users')
+total1 = cursor.fetchone()[0]
+cursor.execute('SELECT SUM(balance) FROM Users')
+total2 = cursor.fetchone()[0]
+
+print(total2/total1)
+
+connection.commit()
+connection.close()
